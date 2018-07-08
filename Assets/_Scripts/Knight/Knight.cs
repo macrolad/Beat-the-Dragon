db65e48aea23fdb6ec5c5877fc0685f24f26ddb9
macrolad <39697678+macrolad@users.noreply.github.com>
2018-07-09 00:06:52 +0200

    Initial commit

---
+++ Assets/_Scripts/Knight/Knight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour {
+
+    public KnightStates KnightState;
+
+    public void EnterState(KnightStates state)
+    {
+        KnightState = state;
+    }
+
+    public delegate void OnStateEnter();
+    public delegate void OnStateStay();
+    public delegate void OnStateExit();
+    public delegate void OnHit();
+    public delegate void OnGetHit(/*DragonState*/);
+    public delegate void OnDeath(/*DragonState*/);
+}
---
+++ Assets/_Scripts/Knight/KnightStates.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightStates : StateMachineBehaviour
+{
+    public virtual void ExitState(Knight knight, KnightStates state)
+    {
+        knight.KnightState = state;
+    }
+
+    //public delegate void OnStateEnter();
+    //public delegate void OnStateStay();
+    //public delegate void OnStateExit();
+    public delegate void OnHit();
+    public delegate void OnGetHit(/*DragonState*/);
+    public delegate void OnDeath(/*DragonState*/);
+}
